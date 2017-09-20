32a54aa1694e0865ff321acf4de0541b2c58a720
Jerry W <jerrywiltse@gmail.com>
2017-09-20 15:31:26 -0400

    cleanup group members

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile, tools, os
+from conans import ConanFile
 
 
 class BoostDisjoint_SetsConan(ConanFile):
@@ -7,8 +7,6 @@ class BoostDisjoint_SetsConan(ConanFile):
     url = "https://github.com/bincrafters/conan-boost-disjoint_sets"
     description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
     requires =  "Boost.Level14Group/1.65.1@bincrafters/testing"
         
     #This library is part of one or more cyclic dependency groups within Boost.
