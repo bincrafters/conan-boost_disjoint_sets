3bb3799ca8fe130eb9e3c7428c677bd46ad99d50
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:55 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostDisjoint_SetsConan(ConanFile):
     name = "boost_disjoint_sets"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_disjoint_sets"
-    
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["disjoint_sets"]
     is_in_cycle_group = True
     is_header_only = True
