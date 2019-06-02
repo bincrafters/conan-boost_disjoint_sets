e14782b5ddca6868b50eac2247c206da16a8b9be
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:11:25 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,13 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostDisjoint_SetsConan(base.BoostBaseConan):
     name = "boost_disjoint_sets"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_disjoint_sets"
-    lib_short_names = ["disjoint_sets"]
-    header_only_libs = ["disjoint_sets"]
-    cycle_group = "boost_cycle_group_d"
-    b2_requires = ["boost_cycle_group_d"]
+    version = "1.70.0"
